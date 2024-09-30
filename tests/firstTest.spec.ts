76ee880c34de8f3e151c8602e81e27ebc110b53d
Jeff Varghese <jeffvarghese007@gmail.com>
2024-09-30 10:03:03 +1000

    added s to line

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,27 @@
+import {test} from '@playwright/test'
+
+
+test.beforeEach(async({page})) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+}
+test.describe.only(('suite1', () => {
+
+
+
+}))
+
+test('the first test', async ({page}) => {
+
+ await page.getByText('Form Layouts').click()
+})
+
+test('navigate to datepicker page', async ({page}) => {
+    
+    await page.getByText('Datepicker').click()
+   })
+
+   test.afterEach(() => {
+
+    
+   })
