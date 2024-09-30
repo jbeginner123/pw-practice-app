cdb3472583e9dbdb0682f27b7c3cceec2d841544
Jeff Varghese <jeffvarghese007@gmail.com>
2024-09-30 11:09:36 +1000

    added s to lines

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,24 +4,8 @@ import {test} from '@playwright/test'
 test.beforeEach(async({page})) => {
     await page.goto('http://localhost:4200/')
     await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+
+
 }
-test.describe.only(('suite1', () => {
 
-
-
-}))
-
-test('the first test', async ({page}) => {
-
- await page.getByText('Form Layouts').click()
-})
-
-test('navigate to datepicker page', async ({page}) => {
-    
-    await page.getByText('Datepicker').click()
-   })
-
-   test.afterEach(() => {
-
-    
-   })
